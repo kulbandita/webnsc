2b4a2233563c6c3437f6ae890a1894d27b58bc69
kulbandita <kulbandita@gmail.com>
2017-02-01 22:56:08 +0700

    summary step

--- js/openwindow.js
+++ js/openwindow.js
@@ -1,4 +1,34 @@
+//function open_calendar_add() {
+//    window.open("calendar_add.html", "_self");
+//}
 
-        function open_calendar_add() {
-            window.open("calendar_add.html", "_self");
-        }
+function openSumStep(num) {
+    switch (num) {
+    case 1:
+        window.location = 'summary_step1.html';
+        break;
+    case 2:
+        window.location = 'summary_step2.html';
+        break;
+    case 3:
+        window.location = 'summary_step3.html';
+        break;
+    case 4:
+        window.location = 'summary_step4.html';
+        break;
+    }
+}
+
+function openPatientStep(num) {
+    switch (num) {
+    case 1:
+        window.location = 'patient_step1.html';
+        break;
+    case 2:
+        window.location = 'patient_step2.html';
+        break;
+    case 3:
+        window.location = 'patient_step3.html';
+        break;
+    }
+}
